033ff898d3237aef82ff1d122a74b5601fc66fa8
YI AI <yi.ai@neaminational.org.au>
2019-03-06 20:40:12 +1100

    update serverless framework to 1.3.8  update react to 16.8.4

--- backend/handler.js
+++ backend/handler.js
@@ -12,6 +12,10 @@ const successfullResponse = {
   body: "Connected"
 };
 
+const jose = require("node-jose");
+const fetch = require("node-fetch");
+fetch.Promise = Bluebird;
+
 module.exports.connectionManager = (event, context, callback) => {
   if (event.requestContext.eventType === "CONNECT") {
     addConnection(event.requestContext.connectionId)
@@ -102,4 +106,87 @@ const deleteConnection = connectionId => {
   };
 
   return DDB.deleteItem(deleteParams).promise();
+};
+
+module.exports.authorizerFunc = async (event, context, callback) => {
+  const keys_url =
+    "https://cognito-idp.ap-southeast-2.amazonaws.com/USER_POOL_ID/.well-known/jwks.json";
+  const {
+    queryStringParameters: { token },
+    methodArn
+  } = event;
+
+  const app_client_id = APP_CLIENT_ID;
+  if (!token) return context.fail("Unauthorized");
+  const sections = token.split(".");
+  let authHeader = jose.util.base64url.decode(sections[0]);
+  authHeader = JSON.parse(authHeader);
+  const kid = authHeader.kid;
+  const rawRes = await fetch(keys_url);
+  const response = await rawRes.json();
+
+  if (rawRes.ok) {
+    const keys = response["keys"];
+    let key_index = -1;
+    keys.some((key, index) => {
+      if (kid == key.kid) {
+        key_index = index;
+      }
+    });
+    const foundKey = keys.find(key => {
+      return kid === key.kid;
+    });
+
+    if (!foundKey) {
+      context.fail("Public key not found in jwks.json");
+    }
+
+    jose.JWK.asKey(foundKey).then(function(result) {
+      // verify the signature
+      jose.JWS.createVerify(result)
+        .verify(token)
+        .then(function(result) {
+          // now we can use the claims
+          const claims = JSON.parse(result.payload);
+          // additionally we can verify the token expiration
+          const current_ts = Math.floor(new Date() / 1000);
+          if (current_ts > claims.exp) {
+            context.fail("Token is expired");
+          }
+          // and the Audience (use claims.client_id if verifying an access token)
+          if (claims.aud != app_client_id) {
+            context.fail("Token was not issued for this audience");
+          }
+          context.succeed(generateAllow("me", methodArn));
+        })
+        .catch(err => {
+          context.fail("Signature verification failed");
+        });
+    });
+  }
+};
+
+const generatePolicy = function(principalId, effect, resource) {
+  var authResponse = {};
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    var policyDocument = {};
+    policyDocument.Version = "2012-10-17"; // default version
+    policyDocument.Statement = [];
+    var statementOne = {};
+    statementOne.Action = "execute-api:Invoke"; // default action
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+  return authResponse;
+};
+
+const generateAllow = function(principalId, resource) {
+  return generatePolicy(principalId, "Allow", resource);
+};
+
+const generateDeny = function(principalId, resource) {
+  return generatePolicy(principalId, "Deny", resource);
 };
